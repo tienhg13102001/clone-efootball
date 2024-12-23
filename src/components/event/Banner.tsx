2d7db32ad20783897817fe23c626cdf64c949cc5
tienhg13102001 <tienhg201@gmail.com>
2024-12-23 17:24:01 +0700

    feat: banner section

--- src/components/event/Banner.tsx
+++ src/components/event/Banner.tsx
@@ -1,10 +1,22 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Banner = () => {
   return (
-    <div className='w-full bg-blue-500'>
-      <div className='max-w-[1250px] bg-red-500 mx-auto h-[500px]'>Banner</div>
-    </div>
+    <div className='w-full bg-black bg-[url(/images/main-bg.webp)] bg-contain bg-center'>
+      <div className=' max-w-[1250px] mx-auto pt-80 pb-[298px] flex flex-col items-center'>
+        <div className='flex flex-col items-center relative drop-shadow-2xl'>
+          <div className='absolute transform -translate-y-[60px] z-10'>
+            <p className='text-center font-GmarketSans font-bold text-[35px] drop-shadow-3xl'>25.01.04 ~ 25.01.26</p>
+            <p className='px-4 pt-2 font-tenada text-4xl bg-gradient-to-l from-[#034230] to-[#00942d] shadow-2xl'>
+              감스트 X 이상호 팀배틀
+            </p>
+          </div>
+          <Image src={"/images/main-title.webp"} alt=';' width={621} height={312} className='drop-shadow-2xl drop' />
+          <p className='font-GmarketSans font-bold text-[#fdd23b] text-[44px] italic drop-shadow-2xl shadow-pink-700'>eFootballAll stars!</p>
+        </div>
+      </div>
+    </div >
   )
 }
 
--- src/components/layout/HeaderEventPage.tsx
+++ src/components/layout/HeaderEventPage.tsx
@@ -1,9 +1,21 @@
+import Image from 'next/image'
 import React from 'react'
 
 
 const HeaderEventPage = () => {
   return (
-    <div className='text-white'>HeaderEventPage</div>
+    <div className='top-0 left-0 right-0 fixed z-50 px-8 py-5 flex justify-between items-start'>
+      <Image src={"/images/football-logo.webp"} alt='ok' width={139} height={139} />
+      <div className='flex items-center text-white gap-10'>
+        <div>
+          <Image src={"/images/menu-logo.webp"} alt='ok' width={79} height={72} />
+        </div>
+        <div className='text-[22px] font-semibold'>이벤트</div>
+        <div  className='text-[22px] font-semibold'>참여방법</div>
+        <div  className='text-[22px] font-semibold'>갤러리</div>
+      </div>
+      <Image src={"/images/konami-logo.webp"} alt='ok' width={170} height={34} />
+    </div>
   )
 }
 
