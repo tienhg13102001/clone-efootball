57bc9f56ec3d41704e206077df61653e43aeb672
tientien1103 <le.nttien99@gmail.com>
2024-12-24 11:12:03 +0700

    feat: participate event, contact form

--- src/components/event/EnterInformation.tsx
+++ src/components/event/EnterInformation.tsx
@@ -1,10 +1,89 @@
+'use client'
+import Image from 'next/image'
 import React from 'react'
+import { useForm } from 'react-hook-form';
 
-
+interface FormData {
+  name: string;
+  phone: string;
+  email: string;
+  condition?: boolean;
+}
 
 const EnterInformation = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const onSubmit = (data: FormData) => console.log(data);
+  console.log(errors);
+
   return (
-    <div>EnterInformation</div>
+    <div className='w-full bg-black min-h-screen bg-[url("/images/information/08-rights-bg.webp")] bg-no-repeat bg-contain h-[1540px]'>
+      <div className='container max-w-[1250px] mx-auto'>
+        <div className='flex flex-col items-center'>
+          <div className='relative mb-[39px]'>
+            <Image src='/images/participate/05-titlebox.webp' alt='' width={110} height={52} />
+            <p className='absolute top-1 left-[33%] text-[#1a1a1a] text-[32px] font-bold'>03</p>
+          </div>
+          <p className='text-white text-[40px] font-bold mb-[35px]'>신청자 정보를 <br />
+            입력해 주세요!
+          </p>
+
+          <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col items-center gap-[18px]'>
+            <input
+              type="text"
+              placeholder="성함"
+              {...register("name", { required: true, maxLength: 80 })}
+              className='text-center text-2xl text-[#b3b3b3] py-6 px-[120px] outline-none'
+            />
+            <input
+              type="tel"
+              placeholder="연락처"
+              {...register("phone", {
+                required: "전화번호는 필수입니다",
+                pattern: {
+                  value: /^01[0-9]{1}-?[0-9]{3,4}-?[0-9]{4}$/,
+                  message: "유효하지 않은 전화번호입니다"
+                },
+                maxLength: {
+                  value: 13,
+                  message: "전화번호는 13자리를 초과할 수 없습니다"
+                }
+              })}
+              className='text-center text-2xl text-[#b3b3b3] py-6 px-[120px] outline-none'
+            />
+            <input
+              type="text"
+              placeholder="이메일"
+              {...register("email", {
+                required: "이메일은 필수입니다",
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  message: "유효하지 않은 이메일 주소입니다"
+                }
+              })}
+              className='text-center text-2xl text-[#b3b3b3] py-6 px-[120px] outline-none'
+            />
+            <div className='flex gap-5 mt-6'>
+              <input
+                type="checkbox"
+                {...register("condition")}
+                className='w-6 h-6'
+              />
+              <p className='text-[#8c8c8c] text-2xl'>개인정보 이용 및 수집에 동의합니다.</p>
+            </div>
+            <button className='mt-16' type="submit">
+              <Image
+                src='/images/participate/05-but.webp'
+                alt='numberbox-1'
+                width={560}
+                height={70}
+                className='object-contain'
+              />
+            </button>
+          </form>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/event/ParticipateEvent.tsx
+++ src/components/event/ParticipateEvent.tsx
@@ -1,10 +1,85 @@
+import Image from 'next/image'
 import React from 'react'
-
-
 
 const ParticipateEvent = () => {
   return (
-    <div>ParticipateEvent</div>
+    <div className='w-full bg-black py-[135px] min-h-screen'>
+      <div className='container max-w-[1250px] mx-auto'>
+        {/* Event Card 1 */}
+        <section className='flex flex-col items-center'>
+          <h3 className='text-[#158145] text-[64px] font-bold mb-[88px]'>이벤트 참여방법</h3>
+          <div className='relative mb-[39px]'>
+            <Image src='/images/participate/05-titlebox.webp' alt='' width={110} height={52} />
+            <p className='absolute top-1 left-[33%] text-[#1a1a1a] text-[32px] font-bold'>01</p>
+          </div>
+          <p className='text-white text-[40px] font-bold mb-[35px]'>나만의 개성 있는 e풋볼컵<br />
+            <span className='text-[#d8f700]'>응원 영상</span>을 올려주세요!</p>
+          <p className='text-[#8c8c8c] text-2xl'>*경기 응원 영상과 관람 영상 모두 올리면 당첨 확률은 업업업!</p>
+
+          <div className='mt-[73px] relative'>
+            <Image src='/images/participate/05-all-box.webp' alt='' width={1250} height={566} className='object-contain' />
+            <div className='absolute top-20 left-[58%]'>
+              <p className='text-white text-[40px] font-bold mb-[69px]'>사전 응원 영상 이벤트 <br />
+                참여 방법</p>
+              <div className='flex gap-3 items-start mb-[53px]'>
+                <Image src='/images/participate/05-numberbox-1.webp' alt='numberbox-1' width={30} height={30} className='object-contain' />
+                <p className='text-white text-[28px] font-bold -translate-y-2'>유튜브 숏츠, 인스타, 틱톡에
+                  <br />
+                  <span className='text-[#d8f700]'>세로용 영상</span>을 업로드하기</p>
+              </div>
+              <div className='flex gap-3 items-start'>
+                <Image src='/images/participate/05-numberbox-2.webp' alt='numberbox-2' width={30} height={30} className='object-contain' />
+                <p className='text-white text-[28px] font-bold mb-[35px] -translate-y-2'><span className='text-[#d8f700]'>URL을 복사</span>하고 <br />
+                  아래에 입력하여 업로드하기</p>
+              </div>
+            </div>
+          </div>
+          <button className='mt-20'>
+            <Image src='/images/participate/05-but.webp' alt='05-but' width={560} height={70} className='object-contain' />
+          </button>
+        </section>
+        {/* Event Card 2 */}
+        <section className='flex flex-col items-center mt-[135px]'>
+          <div className='relative mb-[39px]'>
+            <Image src='/images/participate/05-titlebox.webp' alt='' width={110} height={52} />
+            <p className='absolute top-1 left-[33%] text-[#1a1a1a] text-[32px] font-bold'>02</p>
+          </div>
+          <p className='text-white text-[40px] font-bold mb-[35px]'>나만의 개성 있는 e풋볼컵<br />
+            <span className='text-[#d8f700]'>응원 영상</span>을 올려주세요!</p>
+          <p className='text-[#8c8c8c] text-2xl'>*경기 응원 영상과 관람 영상 모두 올리면 당첨 확률은 업업업!</p>
+
+          <div className='mt-[73px] relative'>
+            <Image src='/images/participate/06-all-box.webp' alt='' width={1250} height={566} className='object-contain' />
+            <div className='absolute top-16 left-[58%]'>
+              <p className='text-white text-[40px] font-bold mb-10'>관람 인증 이벤트 <br />
+                참여 방법</p>
+              <div className='flex gap-3 items-start mb-10'>
+                <Image src='/images/participate/05-numberbox-1.webp' alt='numberbox-1' width={30} height={30} className='object-contain' />
+                <p className='text-white text-[28px] font-medium -translate-y-2'>SOOP 방송 시청 화면과 <br />
+                  응원하는 모습
+                  <span className='text-[#d8f700]'>캡쳐하기</span></p>
+              </div>
+              <div className='flex gap-3 items-start mb-10'>
+                <Image src='/images/participate/05-numberbox-2.webp' alt='numberbox-2' width={30} height={30} className='object-contain' />
+                <p className='text-white text-[28px] font-medium -translate-y-2'>유튜브 숏츠, 인스타, 틱톡에 <br />
+                  <span className='text-[#d8f700]'>세로용 영상</span>을 업로드하기</p>
+              </div>
+
+              <div className='flex gap-3 items-start'>
+                <Image src='/images/participate/06-numberbox-3.webp' alt='numberbox-3' width={30} height={30} className='object-contain' />
+                <p className='text-white text-[28px] font-medium -translate-y-2'>
+                  <span className='text-[#d8f700]'>URL을 복사</span>하여 업로드하기</p>
+              </div>
+            </div>
+
+
+          </div>
+          <button className='mt-10'>
+            <Image src='/images/participate/05-but.webp' alt='05-but' width={560} height={70} className='object-contain' />
+          </button>
+        </section>
+      </div>
+    </div>
   )
 }
 
