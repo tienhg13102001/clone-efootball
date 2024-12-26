5681640e00db2996e2f4d51d2c30264eb297a12d
tientien1103 <le.nttien99@gmail.com>
2024-12-26 11:54:09 +0700

    fix: text-size

--- src/app/(home)/_components/EventCard.tsx
+++ src/app/(home)/_components/EventCard.tsx
@@ -36,7 +36,7 @@ const EventCard = () => {
       <div className="container max-w-[1000px] 3xl:max-w-[1250px] mx-auto">
         <div className='pt-6 mb-[76px] bg-[url("/images/02-box-patterns.webp")] bg-contain bg-center bg-no-repeat rounded-3x'>
           <div className="flex flex-col items-centerl relative">
-            <div className="flex flex-col items-center text-xl lg:text-5xl 3xl:text-[66px] lg:leading-[76px]">
+            <div className="flex flex-col items-center text-lg lg:text-5xl 3xl:text-[66px] lg:leading-[76px]">
               <div className="font-GmarketSans text-center font-medium mt-36 md:mt-[167px]">
                 e풋볼컵 생방송 관람 인증샷 올리고
                 <p className="font-bold text-[#d8f700]">친구랑 맨체스터로 여행 가자!</p>
@@ -54,8 +54,8 @@ const EventCard = () => {
         </div>
         {/*  */}
         <div className='pt-6 mb-[76px] max-w-[1250px] bg-[#131313] bg-[url("/images/03-box-patterns.webp")] bg-cover bg-center bg-no-repeat mx-auto flex flex-col items-center rounded-3xl relative'>
-          <p className="mt-[92px] mb-[37px] text-4xl lg:text-[50px]/[1] font-bold">EVENT 01</p>
-          <div className="flex flex-col items-center text-lg lg:text-3xl 3xl:text-[48px] 3xl:leading-[55px] -tracking-[0.8]">
+          <p className="mt-[92px] mb-[37px] text-3xl md:text-4xl lg:text-[50px]/[1] font-bold">EVENT 01</p>
+          <div className="flex flex-col items-center text-sm xxs:text-base md:text-lg lg:text-3xl 3xl:text-[48px] 3xl:leading-[55px] -tracking-[0.8]">
             <p className="font-GmarketSans text-center font-medium ">
               감스트 X 이상호 팀 배틀 e풋볼을 기다리며
               <br />
@@ -84,7 +84,7 @@ const EventCard = () => {
               })}
             </div>
             <Image src={"/images/03-logo.webp"} alt="" width={isMobile ? 200 : 281} height={34} className="object-contain" />
-            <div className="text-center text-base lg:text-[28.5px]/[1.33] mt-[14px] mb-[109px]">
+            <div className="text-center text-xs xxs:text-sm md:text-base lg:text-[28.5px]/[1.33] mt-[14px] mb-[109px]">
               <p className="font-light">나만의 특별한 응원법! 이 경기장의 패셔니스타는 바로나!</p>
               <p className="font-light">응원할 때 먹으면 두 배 즐거워지는 먹방 노하우!</p>
               <p className="font-bold">특별한 응원 영상 인증과 함께 다양하고 푸짐한 혜택도 팡팡!</p>
@@ -100,12 +100,12 @@ const EventCard = () => {
         </div>
         {/*  */}
         <div className='pt-6 max-w-[1250px] bg-[#131313] bg-[url("/images/04-box-patterns.webp")] bg-cover bg-center mx-auto flex flex-col items-center rounded-3xl relative'>
-          <p className="mt-[92px] mb-[37px] text-4xl lg:text-[50px]/[1] font-bold">EVENT 02</p>
+          <p className="mt-[92px] mb-[37px] text-3xl md:text-4xl lg:text-[50px]/[1] font-bold">EVENT 02</p>
           <div className="flex flex-col items-center text-4xl lg:text-[48px] tracking-[0.8]">
-            <div className="font-GmarketSans text-center font-medium text-lg lg:text-[40px]/[48px]">
+            <div className="font-GmarketSans text-center font-medium text-base xxs:text-lg lg:text-[40px]/[48px]">
               <p>정정당당 e풋볼 올스타즈 본방 시청하며</p>
               <p className="text-[#d8f700] font-bold">응원 영상 및 인증샷 올리기!</p>
-              <p className="text-[#8c8c8c] text-sm lg:text-2xl/[48px]">대회 생방송 : 1월 25일 숲(SOOP) & 유튜브 라이브</p>
+              <p className="text-[#8c8c8c] text-xs md:text-sm lg:text-2xl/[48px]">대회 생방송 : 1월 25일 숲(SOOP) & 유튜브 라이브</p>
             </div>
             <Image src={"/images/04-img.webp"} alt="" width={isLapTop ? 637 : 787} height={715} className="mt-[79px] mb-[140px] object-contain" />
           </div>
